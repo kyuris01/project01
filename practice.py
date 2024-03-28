2f9222d06e9aec03d5937cf1ea868f1c6d24d99b
Chris Ahn <chrisahn1007@gmail.com>
2024-03-28 12:52:42 +0900

    3/28

--- practice.py
+++ practice.py
@@ -3,10 +3,12 @@ import requests
 champion = []
 datas = requests.get("https://ddragon.leagueoflegends.com/cdn/14.6.1/data/en_US/champion.json")
 datas = datas.json()
-print(datas["data"]["Aatrox"]["tags"])
+for data in datas["data"]:
+    champion.append(data)
+print(champion)
 """
 for champ in champion:
-    save_path = "C:\\Users\\window10\\OneDrive\\바탕 화면\\Portfolio\\project01\\static\\img\\champion_img\\" + champ + ".png"
+    save_path = "C:\\Users\\window10\\OneDrive\\諛뷀깢 �솕硫�\\Portfolio\\project01\\static\\img\\champion_img\\" + champ + ".png"
     image_url = "http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champ + ".png"
     download_image = requests.get(image_url)
     
--- server.py
+++ server.py
@@ -12,17 +12,34 @@ app.register_blueprint(view.routing_object, url_prefix='/routing')
 
 @app.route("/")
 def index():
-    champion = []
     datas = requests.get("https://ddragon.leagueoflegends.com/cdn/14.6.1/data/en_US/champion.json")
     datas = datas.json()
+    champion_name=[]
     for data in datas["data"]:
-        champion.append(data) #champion = ["Aatrox", "Ahri",...]   
-    image_url = []
-    for champ in champion:
-        image_url.append("http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champ + ".png") #리스트가 빈 리스트로 초기화되었을 경우, 리스트의 인덱스에
-                                                                                                            #직접값을 할당할수없다. 대신 append()메서드를 이용!
+        champion_name.append(data)
+    Fighter =[]
+    Tank =[]
+    Mage=[]
+    Assassin=[]
+    Marksman=[]
+    Support=[]
+    for i in range(len(datas["data"])):
+        if "Fighter" in datas["data"][champion_name[i]]["tags"]:
+            Fighter.append("http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champion_name[i] + ".png")
+        if "Tank" in datas["data"][champion_name[i]]["tags"]:
+            Tank.append("http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champion_name[i] + ".png")
+        if "Mage" in datas["data"][champion_name[i]]["tags"]:
+            Mage.append("http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champion_name[i] + ".png")
+        if "Assassin" in datas["data"][champion_name[i]]["tags"]:
+            Assassin.append("http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champion_name[i] + ".png")    
+        if "Marksman" in datas["data"][champion_name[i]]["tags"]:
+            Marksman.append("http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champion_name[i] + ".png")
+        if "Support" in datas["data"][champion_name[i]]["tags"]:
+            Support.append("http://ddragon.leagueoflegends.com/cdn/12.7.1/img/champion/" + champion_name[i] + ".png")
+                                                                                                         #리스트가 빈 리스트로 초기화되었을 경우, 리스트의 인덱스에
+                                                                                                        #직접값을 할당할수없다. 대신 append()메서드를 이용!
 
-    return render_template('main.html', image_url=image_url)
+    return render_template('main.html', Fighter=Fighter, Tank=Tank, Mage=Mage, Assassin=Assassin, Marksman=Marksman, Support=Support)
 #다음할것:챔피언 역할별로 이미지분류해서 html로 보내기
 @app.route("/hello")
 def hello():
